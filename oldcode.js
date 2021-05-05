681b7a54a943d8f10ae3c6ce52a863b3765c3eaa
Iryna Stein <isophiestein@gmail.com>
2021-05-05 16:18:41 -0600

    Redid the lab

--- oldcode.js
+++ oldcode.js
@@ -31,36 +31,3 @@
 //       moveDodgerRight();
 //     }
 //   }); 
-
-
-// New code (same as old) written during Phase I
-function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
-    dodger.style.left = `${left - 5}px`;
-  }
-};
-
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-  }
-}); 
-
-
-function moveDodgerRight(){
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
-
-  if (left < 400-40) {
-  dodger.style.left = `${left + 5}px`
-  };
-};
-
-document.addEventListener("keyup", function(e) {
-  if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
-}); 
