48bc0896f244ebb115657e4453b79661a01b3ad5
Iryna Stein <isophiestein@gmail.com>
2021-05-05 16:16:38 -0600

    commit tree 701eb951bac321d290c3bd3492141b16d6ce8d16

---
+++ oldcode.js
@@ -0,0 +1,33 @@
+// // OLD CODE from pre-work
+// const dodger = document.getElementById('dodger')
+
+// dodger.style.backgroundColor = "#FF69B4"
+
+// function moveDodgerLeft() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
+  
+//     if (left > 0) {
+//       dodger.style.left = `${left - 5}px`;
+//     }
+//   };
+
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowLeft") {
+//       moveDodgerLeft();
+//     }
+//   }); 
+
+// function moveDodgerRight (){
+//     const leftNumbers = dodger.style.left.replace("px"," ");
+//     const left = parseInt(leftNumbers, '10');
+//     if (left < 400-40){
+//         dodger.style.left = `${left+5}px`;
+//     }
+// };
+
+// document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowRight") {
+//       moveDodgerRight();
+//     }
+//   }); 
